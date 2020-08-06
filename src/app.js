4e7497f632bbfd0f66cd75fa6be3a09a47c08ccb
James Singletary <jsingletaryiv@gmail.com>
2020-08-06 07:07:16 -0400

    installed and intergrated MDB into current app / Cleaned up and organized the App file structure

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// TODO: Determine the full list of components used and import them via `.module`  
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+
+const App = () => {
+  return (
+    <h1>You Ain't No Rookie... Coins</h1>
+  )
+}
+
+export default App;
