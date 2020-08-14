862d1741730eb1b74b97a92e99c46eaee0d98350
James Singletary <jsingletaryiv@gmail.com>
2020-08-14 17:18:14 -0400

    More styling updates / Removed `<React.StrictMode>` to suppress known console errors in MDB Components

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import * as serviceWorker from './serviceWorker';
 import App from './app';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
